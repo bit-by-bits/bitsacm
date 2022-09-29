0a8f16b24aff1ab40842ed7504bf410efedd20c9
Prateek Kashyap <kashyapprateek13@gmail.com>
2022-09-30 00:21:20 +0530

    feat: add separate functions for urls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, useMediaQuery } from "@chakra-ui/react";
+import GitHub from "./Components/GitHub";
 import NavBar from "./Components/NavBar";
 
+function openGitHub() {
+  window.location = `https://github.com/bitsacm`;
+}
+
 function App() {
+  window.scrollTo(0, 0);
+
   const heading = `<acm bits pilani />`;
   const text = `a group of nerds and tech enthusiasts who push the limits of software development, artificial intelligence, theoretical computing, design and research.`;
 
+  const [isLargerThan1400] = useMediaQuery("(min-width: 1400px)");
+
   return (
-    <Box p={2} height="100vh" width="100vw" overflow="hidden">
+    <Box p={3} height="100vh" width="100vw" overflow="hidden">
       <Box
         borderRadius="50px"
         height="100%"
@@ -18,8 +27,7 @@ function App() {
       >
         <NavBar text="white" />
         <Flex
-        height="80%"
-
+          height="80%"
           direction="column"
           alignItems="center"
           justifyContent="center"
@@ -28,21 +36,35 @@ function App() {
           <Text
             fontFamily="Arial"
             fontWeight="700"
-            fontSize="80px"
+            fontSize="7xl"
             letterSpacing="-4px"
           >
             {heading}
           </Text>
+
           <Text
-          width="50vw"
-textAlign='center'
-            fontFamily="Manrope"
+            width={isLargerThan1400 ? "46vw" : "50vw"}
+            textAlign="center"
             fontWeight="600"
-            fontSize="26px"
-            letterSpacing="-1px"
+            fontSize={isLargerThan1400 ? "2xl" : "xl"}
+            letterSpacing="-0.5px"
           >
             {text}
           </Text>
+
+          <Button
+            onClick={openGitHub}
+            cursor="pointer"
+            leftIcon={<GitHub />}
+            size="sm"
+            colorScheme="gray"
+            mt={6}
+            borderRadius="150px"
+          >
+            <Text fontWeight="700" fontSize="md" color="#7D7D7D">
+              GitHub
+            </Text>
+          </Button>
         </Flex>
       </Box>
     </Box>
